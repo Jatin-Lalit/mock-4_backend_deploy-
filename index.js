37cac060b431cd85e4dc5923aee1790effcc9574
unknown <freemindedjatin@gmail.com>
2023-08-17 18:19:20 +0530

    ok

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const express=require("express");
 const {connect}=require("./db")
 const app=express();
 const{tripRouter}=require("./routers/trip")
-
+const cors = require('cors');
 
 app.use(express.json());
 
 app.use("/",tripRouter)
 
-
+app.use(cors());
 
 app.listen(8998,async()=>{
     try{
